c6707fb82131414898a5fec6b0521441690c8007
Anthony Rodriguez <anthonyr38@proton.me>
2023-05-15 15:46:41 -0400

    Sel. Statements Exercise 1

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+            //Exercise 1 Guessing Game
+            var r = new Random();
+            var favNumber = r.Next(1, 10);
+
+
+            Console.WriteLine("Guess the number.");
+            Console.WriteLine("Pick a number from 1-10.");
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favNumber)
+            {
+                Console.WriteLine("Too low.");
+            }
+            else if (userInput > favNumber) 
+            {
+                Console.WriteLine("Too high.");
+            }
+            else 
+            {
+                Console.WriteLine("You Guessed It!!!");
+            }
+        }   
     }
 }
